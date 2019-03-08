de24ff058478443e447aa8a5af1a47928f52b97e
ShunyuYao <ysy2017@sjtu.edu.cn>
2019-03-08 12:31:33 +0800

    training test

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-source activate tensorflow
-python training.py --run_name=BlitzNet300_dishes --dataset=coco-seg --trunk=resnet50 --x4 --batch_size=16 --optimizer=adam --detect --segment --max_iterations=65000 --lr_decay 40000 50000 --ckpt=11000
+source activate tf1.12
+python training.py --run_name=BlitzNet300_instance --dataset=coco-seg --trunk=resnet50 --x4 --batch_size=8 --optimizer=adam --detect --segment --instance --max_iterations=65000 --lr_decay 40000 50000 --instance_num 30
